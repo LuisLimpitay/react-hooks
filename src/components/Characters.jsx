95d461c0ff7f6045ee4dbf94f5d342e10b267981
luchin <luchinaje@gmail.com>
2021-08-22 23:18:27 -0300

    useMemo

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,52 +1,69 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useMemo } from "react";
 
 const initialState = {
-  favorites: []
-}
+  favorites: [],
+};
 
 const favoriteReducer = (state, action) => {
   switch (action.type) {
-    case 'ADD_TO_FAVORITE':
+    case "ADD_TO_FAVORITE":
       return {
         ...state,
-        favorites: [...state.favorites, action.payload]
+        favorites: [...state.favorites, action.payload],
       };
     default:
       return state;
   }
-}
+};
 
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
   const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
-
+  //1
+  const [search, setSearch] = useState("");
   useEffect(() => {
-    fetch('https://rickandmortyapi.com/api/character/')
-      .then(response => response.json())
-      .then(data => setCharacters(data.results));
+    fetch("https://rickandmortyapi.com/api/character/")
+      .then((response) => response.json())
+      .then((data) => setCharacters(data.results));
   }, []);
 
-  const handleClick = favorite => {
-    dispatch({ type: 'ADD_TO_FAVORITE', payload: favorite })
-  }
+  const handleClick = (favorite) => {
+    dispatch({ type: "ADD_TO_FAVORITE", payload: favorite });
+  };
+
+  //2
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
+  //4 crear filtros
+  const filteredUsers = useMemo(() => characters.filter((user) => 
+      {
+        return user.name.toLowerCase().includes(search.toLowerCase());
+      }),
+    [characters, search]
+  );
 
   return (
     <div className="Characters">
-
-      {favorites.favorites.map(favorite => (
-        <li key={favorite.id}>
-          {favorite.name}
-        </li>
+      {favorites.favorites.map((favorite) => (
+        <li key={favorite.id}>{favorite.name}</li>
       ))}
-
-      {characters.map(character => (
+      //3 agrega input, usa el onChange por que cuando se termine de tipear
+      desencadena el llamado
+      <div className="Search">
+        <input type="text" value={search} onChange={handleSearch} />
+      </div>
+      {filteredUsers.map((character) => (
         <div className="item" key={character.id}>
           <h2>{character.name}</h2>
-          <button type="button" onClick={() => handleClick(character)}>Agregar a Favoritos</button>
+          <button type="button" onClick={() => handleClick(character)}>
+            Agregar a Favoritos
+          </button>
         </div>
       ))}
     </div>
   );
-}
+};
 
 export default Characters;
